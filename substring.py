a134bc47d135bad6f119500165133a0f89c3bfed
Vivek Kumar <85937313+Vivekkumar6769@users.noreply.github.com>
2022-09-12 19:11:59 +0530

    Add files via upload

---
+++ substring.py
@@ -0,0 +1,8 @@
+def main():
+    str1=input("Enter the String\n")
+    str2=input("Enter the Sub-String\n")
+    if (str1.find(str2)==-1):
+        print("String not available\n")
+    else:
+        print("String available")
+main()
---
+++ ttt.py
@@ -0,0 +1,6 @@
+from tkinter import *
+window = Tk()
+label = Label(window, text = "Welcome to Python")
+button = Button(window, text = "Click Me")
+label.pack()
+button.pack()
